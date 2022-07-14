36e117052bff3f240f25a3ac925c1eb725e5ebe1
Sarah Dhar <105319763+Angamid@users.noreply.github.com>
2022-07-14 15:51:35 -0500

    Update divvy.py
    Added a function to extract the names of the two people who are sharing expenses from the google sheet.

--- divvy.py
+++ divvy.py
@@ -28,6 +28,26 @@ def get_sheet_data():
     sheet_instance = sheet.get_worksheet(0)
     df = pd.DataFrame(sheet_instance.get_all_records())
     return df
+
+
+def get_names(df):
+    """This function will extract the names of the two people who need to share expenses"""
+    # Have regex pull out the names of whoever is in the list in order to make sure the input names match.
+    new_list = []
+    result = df['Name'].str.extract(r'([A-Z][a-z]*)')
+    # get the values out of the dataframe
+    vals = result.values
+    # add those values into a list of lists, each list a single name instance.
+    name_list = vals.tolist()
+    # extract each list instance of the name to make a flat list of the names.
+    for name in name_list:
+        for n in name:
+            new_list.append(n)
+    # make the list into a set so that it removes all repeats and leaves you with the two names you need.
+    name_set = set(new_list)
+    print(name_set)
+    p_one, p_two = name_set
+    return p_one, p_two, df
     
 if __name__ == '__main__':
   main()
