b921f999b8fd5ad8d275b405a14dc456f3b07110
Sarah Dhar <105319763+Angamid@users.noreply.github.com>
2022-07-14 15:43:29 -0500

    Create divvy.py
    Adding in the main function of the python file.

---
+++ divvy.py
@@ -0,0 +1,22 @@
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from fpdf import FPDF
+from datetime import date
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+def main():
+    df = get_sheet_data()
+    names = get_names(df)
+    total_sum = sum_of_cost(names)
+    owes = who_owes(total_sum)
+    pdf = create_pdf(owes)
+    send_email(pdf)
+    
+if __name__ == '__main__':
+  main()
