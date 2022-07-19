a7cd4f8eaff55cf89a3e6a687be6dae975a5bbeb
Sarah Dhar <105319763+Angamid@users.noreply.github.com>
2022-07-18 23:40:22 -0500

    Update divvy.py
    Final update to divvy.py. Deleted comments, updated code, added a few lines here and there.

--- divvy.py
+++ divvy.py
@@ -20,7 +20,7 @@ def main():
 
     
 def get_sheet_data():
-
+    """This function extracts the data from the shared Google sheet and converts it into a dataframe for easier manipulation."""
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('<INSERT_JSON_FILE_NAME_HERE>', scope)
     client = gspread.authorize(creds)
@@ -32,18 +32,13 @@ def get_sheet_data():
 
 def get_names(df):
     """This function will extract the names of the two people who need to share expenses"""
-    # Have regex pull out the names of whoever is in the list in order to make sure the input names match.
     new_list = []
     result = df['Name'].str.extract(r'([A-Z][a-z]*)')
-    # get the values out of the dataframe
     vals = result.values
-    # add those values into a list of lists, each list a single name instance.
     name_list = vals.tolist()
-    # extract each list instance of the name to make a flat list of the names.
     for name in name_list:
         for n in name:
             new_list.append(n)
-    # make the list into a set so that it removes all repeats and leaves you with the two names you need.
     name_set = set(new_list)
     p_one, p_two = name_set
     return p_one, p_two, df
@@ -52,33 +47,25 @@ def get_names(df):
 def sum_of_cost(names_tuple):
     """This function adds up the total each person spent based on their name and if it is an item that needs to be included in the total."""
     p_one, p_two, df = names_tuple
-    # get the total spent per person from the dataframe.
     total_one = df[(df['Name']==p_one) & (df['Add to total']=='Yes')]['Cost'].sum()
     total_two = df[(df['Name']==p_two) & (df['Add to total']=='Yes')]['Cost'].sum()
     items_one = df[(df['Name']==p_one)][['Item', 'Cost']]
     items_two = df[(df['Name']==p_two)][['Item', 'Cost']]
     print(items_one.to_string())
     print(items_two.to_string())
-    # split the string of costs into a list
     list_total_one = total_one.split('$')
     list_total_two = total_two.split('$')
     list_total_one.remove('')
     list_total_two.remove('')
-    # add the sum of items for each person.
     total_cost_one = sum(float(sub) for sub in list_total_one)
     total_cost_two = sum(float(sub) for sub in list_total_two)
-    # return a tuple of first sum, second sum, person one, and person two
     return total_cost_one, total_cost_two, p_one, p_two, items_one.to_string(header=None, index=False), items_two.to_string(header=None, index=False)
 
 
 def who_owes(total_cost):
     """This function compares the totals and takes the difference, divides it in half, and decides who needs to pay who for the half-difference."""
-    # amount_owed = 0
-    # unpack the tuple from the sum_of_cost() function.
     total_one, total_two, p_one, p_two, items_one, items_two = total_cost
-    # figure out who spent more of the two people
     if total_one > total_two:
-        # find the difference between the two totals and divide the difference by two.
         amount_owed = (total_one - total_two)/2
         return ("${:0.2f} is what is owed to {}.".format(amount_owed, p_one.title()),
                 "{} spent ${:0.2f} this month.".format(p_one.title(), total_one),
@@ -95,29 +82,29 @@ def who_owes(total_cost):
 def create_pdf(output):
     """This function puts the results into a PDF and saves it as the date this script is run."""
     owed, first_person, second_person, items_first, items_second = output
-    # get the date for today
+    folder_path = '<INSERT FULL PATH TO FOLDER HERE>'
     today = date.today()
-    # create pdf with input from previous functions to be saved as today's date.
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size = 15)
-    pdf.cell(200, 10, txt = "Monthly Expenses", ln = 1, align = 'C')
+    pdf.cell(200, 10, txt = "Monthly Expenses for Vivonuo", ln = 1, align = 'C')
     pdf.cell(200, 10, txt = "", ln = 1, align = 'C')
+    pdf.set_font("Arial", size = 10)
+    pdf.multi_cell(200, 5, items_first, align = 'L')
     pdf.set_font("Arial", size = 12)
     pdf.cell(200, 10, txt = first_person, ln = 1, align = 'L')
+    pdf.cell(200, 5, txt="----------------------------------------------", ln=1, align='L')
+    pdf.cell(200, 5, txt="", ln=1, align='C')
     pdf.set_font("Arial", size = 10)
-    pdf.multi_cell(200, 10, items_first, align = 'L')
+    pdf.multi_cell(200, 5, items_second, align = 'L')
     pdf.set_font("Arial", size = 12)
     pdf.cell(200, 10, txt = second_person, ln = 1, align = 'L')
-    pdf.set_font("Arial", size = 10)
-    pdf.multi_cell(200, 10, items_second, align = 'L')
     pdf.set_font("Arial", size = 12)
-    pdf.cell(200, 10, txt = "----------------------------------------------", ln = 1, align = 'L')
+    pdf.cell(200, 5, txt = "----------------------------------------------", ln = 1, align = 'L')
     pdf.cell(200, 10, txt = owed, ln = 1, align = 'L')
     pdf.cell(200, 10, txt = "", ln = 1, align = 'C')
     pdf.cell(200, 10, txt = "Be sure to keep your receipts in a safe place!", ln = 1, align = 'L')
-    pdf.output("{}.pdf".format(today))
-    return "{}.pdf".format(today)
+    pdf.output("{}{}.pdf".format(folder_path, today))
 
 
 def send_email(file_name):
@@ -125,16 +112,17 @@ def send_email(file_name):
     message_content = '''Here is the spending report for Vivonuo this month.
 Please send the money as soon as you are able and don't forget to keep your receipts in a safe place.
 
-    Thank you and have a great day!'''
+Thank you and have a great day!'''
 
     sender = 'YOUR EMAIL'
     password = 'YOUR PASSWORD/APP PASSWORD'
     receiver = 'RECIPIENT'S EMAIL'
+    today = date.today()
 
     message = MIMEMultipart()
     message['From'] = sender
     message['To'] = receiver
-    message['Subject'] = 'A test email from me to me.'
+    message['Subject'] = 'Expense report for {}'.format(today)
 
     message.attach(MIMEText(message_content, 'plain'))
     attach_file = open(file_name, 'rb')
@@ -145,13 +133,19 @@ Please send the money as soon as you are able and don't forget to keep your rece
     payload.add_header('Content-Decomposition', 'attachment', filename=file_name)
     message.attach(payload)
 
-    session = smtplib.SMTP('smtp.gmail.com', 587)
-    session.starttls()
-    session.login(sender, password)
-    text = message.as_string()
-    session.sendmail(sender, receiver, text)
-    session.quit()
-    print('Mail has been sent.')
+    try:
+        session = smtplib.SMTP('smtp.gmail.com', 587)
+        session.starttls()
+        session.login(sender, password)
+        text = message.as_string()
+        session.sendmail(sender, receiver, text)
+        session.quit()
+        print('The email has been sent.')
+        # uncomment the next line when troubleshooting and need to time the program.
+        # print(time.time() - start_time, 's')
+    except Exception as e:
+        print('Something went wrong. The file was saved to your computer, but the email was not sent because of \n'
+              '{}'.format(e))
 
     
 if __name__ == '__main__':
